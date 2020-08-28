e67db37e767d08b8ef21f88f0df234f2acc7a585
Fran <fmjerez@icloud.com>
2020-08-28 14:05:54 +0200

    CSS done

--- src/components/Quotation.js
+++ src/components/Quotation.js
@@ -1,25 +1,46 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const ResultDiv = styled.div`
+  color: #fff;
+  font-family: Arial, Helvetica, sans-serif;
+`;
+
+const Info = styled.p`
+  font-size: 18px;
+
+  span {
+    font-weight: bold;
+  }
+`;
+
+const Price = styled.p`
+  font-size: 30px;
+  span {
+    font-weight: bold;
+  }
+`;
 
 const Quotation = ({ result }) => {
   if (Object.keys(result).length === 0) return null;
   return (
-    <div>
-      <p>
+    <ResultDiv>
+      <Price>
         The price is: <span>{result.PRICE}</span>
-      </p>
-      <p>
+      </Price>
+      <Info>
         Highest price of day: <span>{result.HIGHDAY}</span>
-      </p>
-      <p>
+      </Info>
+      <Info>
         Lowest price of day: <span>{result.LOWDAY}</span>
-      </p>
-      <p>
+      </Info>
+      <Info>
         Variation last 24 hours: <span>{result.CHANGEPCT24HOUR}</span>
-      </p>
-      <p>
+      </Info>
+      <Info>
         Last update: <span>{result.LASTUPDATE}</span>
-      </p>
-    </div>
+      </Info>
+    </ResultDiv>
   );
 };
 
